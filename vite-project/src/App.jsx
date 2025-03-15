e53ca1bc7fe5eaed5d8abd38a5567f85550a2d90
kevinbrinsly <brinslykevin@gmail.com>
2025-03-15 16:14:48 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import About from './components/about';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Skills from './components/skills';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
